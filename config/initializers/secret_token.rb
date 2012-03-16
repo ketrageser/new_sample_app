946b4cbe975f8040ffc1b87664db248a46bb08e4
Kelly Trageser <kelty@cablespeed.com>
2012-03-16 09:45:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b810f523f519b1d2c97d77244aeb50ce5a9091898b160ff31258a770d87661b3a3b24bd166b39b30c9590a5e37c20ded049df9a37e6035750ce1f1005b4a120b'
